654e3ac14866f7323b8ded914187579e77f8a7c1
kiarash <kiarashmohsenipoor99@gmail.com>
2022-02-04 00:58:26 +0330

    make a form and give them a little css style

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,14 +1,20 @@
+import Card from "../UI/Card";
+import styles from "../Users/AddUser.module.css";
+
 const AddUser = (props) => {
     const addUserHandler = (event) => {
         event.preventDefault();
     };
     return (
-        <form onSubmit={addUserHandler}>
-            <label htmlFor="name">Name</label>
-            <input id="name" type="text"/>
-            <label htmlFor="age">Age</label>
-            <input id="age" type="number"/>
-        </form>
+        <Card className={styles.input}>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="name">Name</label>
+                <input id="name" type="text"/>
+                <label htmlFor="age">Age</label>
+                <input id="age" type="number"/>
+                <button type={"submit"}>Add User</button>
+            </form>
+        </Card>
     );
 }
 
