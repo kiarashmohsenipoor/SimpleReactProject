96530b763c1a110e2e01f2faa3dde33d42aea608
kiarash <kiarashmohsenipoor99@gmail.com>
2022-02-04 02:42:02 +0330

    make a list of user by input

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import React, {useState} from "react";
 import './App.css';
 import AddUser from "./Components/Users/AddUser";
+import UserList from "./Components/Users/UserList";
 
 function App() {
-  return (
-    <div className="App">
-      <AddUser />
-    </div>
-  );
+    const [userList, setUserList] = useState([]);
+    const addUserHandler = (userName, userAge) => {
+        setUserList(
+            (prevUserList) => {
+                return [...prevUserList, {name: userName, age: userAge, id: Math.random().toString()}];
+            }
+        );
+    };
+
+    return (
+        <div className="App">
+            <AddUser onAddUser={addUserHandler}/>
+            <UserList users={userList}/>
+        </div>
+    );
 }
 
 export default App;
--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -15,7 +15,7 @@ const AddUser = (props) => {
         if (enteredAge < 1) {
             return;
         }
-        console.log(enteredName, enteredAge);
+        props.onAddUser(enteredName, enteredAge);
         setEnteredName('');
         setEnteredAge('');
     };
